32c06ba6e41b17e9a8800cc5ca8ae43941b94bd4
baemgao <bhduc264@gmail.com>
2023-10-08 11:17:10 +0700

    update

--- lib/representation/screens/home_screen.dart
+++ lib/representation/screens/home_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo_02/core/helpers/asset_helpers.dart';
-import 'package:flutter_demo_02/core/helpers/image_helper.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   static const routName = 'home_screen';
   @override
@@ -11,29 +9,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final PageController _pageController = PageController();
-
-  Widget _buildItemInHomeScreen(String image) {
-    return Column(
-      children: [
-        ImageHelper.loadFormAsset(
-          image,
-          height: 300,
-          fit: BoxFit.fitWidth,
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: [
-          _buildItemInHomeScreen(AssetHelper.imageBanner),
-        ],
-      ),
-    );
+    return Container();
   }
 }
+
