587c1dd1396fd1829589835a6cb122daf2741dc5
maotou-spy <phucbuihuu2604@gmail.com>
2023-10-17 21:47:22 +0700

    change device

--- lib/representation/screens/account_routes/account_screen.dart
+++ lib/representation/screens/account_routes/account_screen.dart
@@ -35,177 +35,180 @@ class _AccountScreenState extends State<AccountScreen> {
       body: Container(
         color: ColorPalette.bgColor,
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Box.sizedBox(10, ColorPalette.secondColor, null),
-              // Phần trên cùng
-              Container(
-                color: ColorPalette.bgColor,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      const Expanded(
-                        flex: 1,
-                        child: CircleAvatar(
-                          radius: 70,
-                          backgroundImage: AssetImage(AssetHelper.imageBanner),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Box.sizedBox(10, ColorPalette.secondColor, null),
+                // Phần trên cùng
+                Container(
+                  color: ColorPalette.bgColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        const Expanded(
+                          flex: 1,
+                          child: CircleAvatar(
+                            radius: 70,
+                            backgroundImage:
+                                AssetImage(AssetHelper.imageBanner),
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Nguyễn Văn A',
-                              style: TextStyle(
-                                color: ColorPalette.textColor,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Box.sizedBox(8, null, null),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.of(context)
-                                    .pushNamed(Profile.routName);
-                              },
-                              child: const Text(
-                                'View Profile',
+                        Expanded(
+                          flex: 1,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Nguyễn Văn A',
                                 style: TextStyle(
-                                  color: ColorPalette.textOnTapColor,
-                                  fontSize: 16,
-                                  decoration: TextDecoration.none,
+                                  color: ColorPalette.textColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),
-                          ],
+                              Box.sizedBox(8, null, null),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context)
+                                      .pushNamed(Profile.routName);
+                                },
+                                child: const Text(
+                                  'View Profile',
+                                  style: TextStyle(
+                                    color: ColorPalette.textOnTapColor,
+                                    fontSize: 16,
+                                    decoration: TextDecoration.none,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Box.sizedBox(10, ColorPalette.secondColor, null),
+
+                MyAccountButton(
+                  text: 'Bills',
+                  leftIcon: FontAwesomeIcons.fileInvoiceDollar,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(MyBillScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                MyAccountButton(
+                  text: 'Requests',
+                  leftIcon: FontAwesomeIcons.notesMedical,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(MyRequestScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                MyAccountButton(
+                  text: 'Apartments',
+                  leftIcon: FontAwesomeIcons.solidBuilding,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(MyApartmentScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                MyAccountButton(
+                  text: 'Contracts',
+                  leftIcon: FontAwesomeIcons.fileContract,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(MyContractScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                MyAccountButton(
+                  text: 'Packages',
+                  leftIcon: FontAwesomeIcons.cubes,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(PackageScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                MyAccountButton(
+                  text: 'Services',
+                  leftIcon: FontAwesomeIcons.ethernet,
+                  rightIcon: Icons.arrow_forward,
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(ServiceScreen.routName);
+                  },
+                ),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(Apartee.routName);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: ColorPalette.secondColor,
+                    surfaceTintColor: ColorPalette.primaryColor,
+                    elevation: 5,
+                    backgroundColor: ColorPalette.bgColor,
+                    minimumSize: const Size(300, 51),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: CircleAvatar(
+                          radius: 15,
+                          backgroundColor: ColorPalette.primaryColor,
+                          child: Image.asset(
+                            'assets/imgs/logo.png',
+                            width: 50,
+                            height: 50,
+                            color: ColorPalette.bgColor,
+                          ),
                         ),
                       ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      const SizedBox(
+                        width: 300,
+                        child: Text(
+                          'About Apartee',
+                          style: TextStyle(
+                              color: ColorPalette.textColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const Expanded(flex: 1, child: SizedBox()),
                     ],
                   ),
                 ),
-              ),
-              Box.sizedBox(10, ColorPalette.secondColor, null),
+                Box.sizedBox(1, ColorPalette.spaceLine, null),
+                Box.sizedBox(20, ColorPalette.bgColor, null),
 
-              MyAccountButton(
-                text: 'Bills',
-                leftIcon: FontAwesomeIcons.fileInvoiceDollar,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MyBillScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              MyAccountButton(
-                text: 'Requests',
-                leftIcon: FontAwesomeIcons.notesMedical,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MyRequestScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              MyAccountButton(
-                text: 'Apartments',
-                leftIcon: FontAwesomeIcons.solidBuilding,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MyApartmentScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              MyAccountButton(
-                text: 'Contracts',
-                leftIcon: FontAwesomeIcons.fileContract,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(MyContractScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              MyAccountButton(
-                text: 'Packages',
-                leftIcon: FontAwesomeIcons.cubes,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(PackageScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              MyAccountButton(
-                text: 'Services',
-                leftIcon: FontAwesomeIcons.ethernet,
-                rightIcon: Icons.arrow_forward,
-                onPressed: () {
-                  Navigator.of(context).pushNamed(ServiceScreen.routName);
-                },
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(Apartee.routName);
-                },
-                style: ElevatedButton.styleFrom(
-                  shadowColor: ColorPalette.secondColor,
-                  surfaceTintColor: ColorPalette.primaryColor,
-                  elevation: 5,
-                  backgroundColor: ColorPalette.bgColor,
-                  minimumSize: const Size(300, 51),
+                //Logout
+                MyButton(
+                  onTap: OnTap.onTap(),
+                  text: 'Logout',
+                  color: ColorPalette.secondColor,
+                  textColor: ColorPalette.primaryColor,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: CircleAvatar(
-                        radius: 15,
-                        backgroundColor: ColorPalette.primaryColor,
-                        child: Image.asset(
-                          'assets/imgs/logo.png',
-                          width: 50,
-                          height: 50,
-                          color: ColorPalette.bgColor,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 25,
-                    ),
-                    const SizedBox(
-                      width: 300,
-                      child: Text(
-                        'About Apartee',
-                        style: TextStyle(
-                            color: ColorPalette.textColor,
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const Expanded(flex: 1, child: SizedBox()),
-                  ],
-                ),
-              ),
-              Box.sizedBox(1, ColorPalette.spaceLine, null),
-              Box.sizedBox(20, ColorPalette.bgColor, null),
-
-              //Logout
-              MyButton(
-                onTap: OnTap.onTap(),
-                text: 'Logout',
-                color: ColorPalette.secondColor,
-                textColor: ColorPalette.primaryColor,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/representation/screens/account_routes/apartee_screen.dart
+++ lib/representation/screens/account_routes/apartee_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_02/components/account_button.dart';
 import 'package:flutter_demo_02/components/app_bar.dart';
 import 'package:flutter_demo_02/core/const/color_const.dart';
+import 'package:font_awesome_icon_class/font_awesome_icon_class.dart';
+
+import 'account_screen.dart';
+
+final Uri _url = Uri.parse('https://flutter.dev');
 
 class Apartee extends StatelessWidget {
   const Apartee({super.key});
@@ -23,6 +29,65 @@ class Apartee extends StatelessWidget {
             //Navigator.of(context).pushNamed(AccountScreen.routName);
           },
         ),
+      ),
+      body: Container(
+        color: ColorPalette.secondColor,
+        child: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+          children: [
+            Box.sizedBox(10, ColorPalette.secondColor, null),
+            MyAccountButton(
+                text: 'Application Infor',
+                leftIcon: FontAwesomeIcons.a,
+                rightIcon: Icons.arrow_forward,
+                onPressed: () {}),
+            Box.sizedBox(5, ColorPalette.secondColor, null),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shadowColor: ColorPalette.secondColor,
+                surfaceTintColor: ColorPalette.primaryColor,
+                elevation: 5,
+                backgroundColor: ColorPalette.bgColor,
+                minimumSize: const Size(300, 51),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: CircleAvatar(
+                      radius: 15,
+                      backgroundColor: ColorPalette.primaryColor,
+                      child: Image.asset(
+                        'assets/imgs/logo.png',
+                        width: 50,
+                        height: 50,
+                        color: ColorPalette.bgColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  const SizedBox(
+                    width: 300,
+                    child: Text(
+                      'Apartee.com.vn',
+                      style: TextStyle(
+                          color: ColorPalette.primaryColor,
+                          fontSize: 17,
+                          fontWeight: FontWeight.normal),
+                    ),
+                  ),
+                  const Expanded(flex: 1, child: SizedBox()),
+                ],
+              ),
+            ),
+            Box.sizedBox(5, ColorPalette.secondColor, null),
+          ],
+        ))),
       ),
     );
   }
